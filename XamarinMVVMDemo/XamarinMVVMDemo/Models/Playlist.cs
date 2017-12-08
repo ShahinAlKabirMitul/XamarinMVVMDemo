003943ad9b4e08530e197fe3e7dd91aa20891f2d
ShahinAlKabirMitul <mitul.cse@gmail.com>
2017-12-08 18:11:43 +0600

    Add Model

---
+++ XamarinMVVMDemo/XamarinMVVMDemo/Models/Playlist.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XamarinMVVMDemo.Annotations;
+
+namespace XamarinMVVMDemo.Models
+{
+    public class Playlist: INotifyPropertyChanged
+
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Title { get; set; }
+
+        private bool _isFavorite;
+        public bool IsFavorite
+        {
+            get { return _isFavorite; }
+            set
+            {
+                if (_isFavorite == value)
+                    return;
+
+                _isFavorite = value;
+
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Color));
+            }
+        }
+
+        public Color Color
+        {
+            get { return IsFavorite ? Color.Pink : Color.Black; }
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
